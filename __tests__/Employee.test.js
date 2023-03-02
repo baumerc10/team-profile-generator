e0a03b5f07377c14fe8d698201344390ee8dbe4c
Cody Kirschbaum <baumerc10@gmail.com>
2023-03-02 10:57:24 -0500

    edited tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,33 +1,30 @@
 const Employee = require ('../lib/Employee');
 
 test('Creates an employee object instance', () => {
-    const employee = new Employee('Cody', 10, 'baumerc10@gmail.com');
-
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    const employee = new Employee();
+    expect(typeof(employee).toEqual('object'));
 });
 
 test('Get employee name', () => {
-    const employee = new Employee('Cody', 10, 'baumerc10@gmail.com');
-
-    expect(employee.getName()).toEqual(expect.any(String));
+    const name = 'Cody';
+    const employee = new Employee(name);
+    expect(employee.getName()).toEqual(name);
 });
 
 test('Get employee ID', () => {
-    const employee = new Employee('Cody', 10, 'baumerc10@gmail.com');
-
-    expect(employee.getId()).toEqual(expect.any(Number));
+    const id = 10;
+    const employee = new Employee('Cody', id);
+    expect(employee.getId()).toEqual(id);
 });
 
 test('Get employee email', () => {
+    const email = 'baumerc10@gmail.com';
     const employee = new Employee('Cody', 10, 'baumerc10@gmail.com');
-
-    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    expect(employee.getEmail()).toEqual(email);
 });
 
 test('Get employee role', () => {
+    const role = 'Employee';
     const employee = new Employee('Cody', 10, 'baumerc10@gmail.com');
-
-    expect(employee.getRole()).toEqual("Employee");
+    expect(employee.getRole()).toEqual(role);
 }); 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,17 @@
 const Manager = require('../lib/Manager');
 
 test('Create a Manager object.', () => {
-    const manager = new Manager('Cody', 10, 'baumerc10@gmail.com', 1);
+    const manager = new Manager('Cody', 10, 'baumerc10@gmail.com', officeNum);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 test('Get Employee role.', () => {
-    const manager = new Manager('Cody', 10, 'baumerc10@gmail.com', 1);
+    const manager = new Manager('Cody', 10, 'baumerc10@gmail.com', officeNum);
     expect(manager.getRole()).toEqual('Manager');
+})
+
+test('Get Manager office number.', () => {
+    const officeNum = 1;
+    const manager = new Manager('Cody', 10, 'baumerc10@gmail.com', officeNum);
+    expect(manager.getOfficeNumber()).toEqual(officeNum);
 })
